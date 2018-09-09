cd6f24709fd845ee0818b160885416dcd9aecae7
s.andriyaka <s.andriyaka@1plus1.tv>
2018-09-09 14:50:18 +0300

    Initial commit Teacher Student

---
+++ univer/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.core.validators import MaxValueValidator
+
+class CommonInfo(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=12)
+
+    class Meta:
+        abstract = True
+
+
+class Teacher(CommonInfo):
+    subject = models.CharField(max_length=50)
+
+class Student(CommonInfo):
+    avg_mark = models.PositiveSmallIntegerField(validators=[MaxValueValidator])
+    # quiz = models.ForeignKey(Quiz,on_delete=models.CASCADE)
+
