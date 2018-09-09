3f575618e9b0cfe75c21996321ba88657898daa6
mrbloom <mrbloom@ukr.net>
2018-09-10 00:19:27 +0300

    part3 done

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('part1/', views.index, name='index'),
+    path('part2/', views.part2, name='part2'),
+    path('part3/', views.part3_index, name='part3_index'),
+    path('part3/<int:question_id>/', views.part3_detail, name='part3_detail'),
+    path('part3/<int:question_id>/results/', views.part3_results, name='part3_results'),
+    path('part3/<int:question_id>/vote/', views.part3_vote, name='part3_vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,70 @@
+import html
 from django.http import HttpResponse
-
+from .models import Question
+from django.template import loader
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def part2(request):
+
+    text = '''<!doctype html>
+    >> > from polls.models import Choice, Question  # Import the model classes we just wrote. <br>
+
+    # No questions are in the system yet. <br>
+    >> > Question.objects.all() <br>''' + html.escape(str(Question.objects.all())) + '<br>'
+
+    text += '''# Create a new Question. <br>
+    # Support for time zones is enabled in the default settings file, so <br>
+    # Django expects a datetime with tzinfo for pub_date. Use timezone.now() <br>
+    # instead of datetime.datetime.now() and it will do the right thing. <br>
+    
+    from django.utils import timezone <br>
+    >> > q = Question(question_text="What's new?", pub_date=timezone.now()) <br>
+
+    # Save the object into the database. You have to call save() explicitly. <br>
+    >> > q.save() <br>
+    '''
+    from django.utils import timezone
+    q = Question(question_text="What's new?", pub_date=timezone.now())
+
+    # Save the object into the database. You have to call save() explicitly. <br>
+    q.save()
+
+    text += str(q)+'<br>'
+
+    text += '''# Now it has an ID. <br>
+    >> > q.id <br>''' + str(q.id) + '<br>' + '''# Access model field values via Python attributes. <br>
+    >> > q.question_text <br>''' +  str(q.question_text) +    ">> > q.pub_date <br>"+str(q.pub_date) + "<br>" + '''
+    # Change values by changing the attributes, then calling save(). <br>
+    >> > q.question_text = "What's up?"
+    >> > q.save()'''
+    q.question_text = "What's up?"
+    q.save()
+
+    text +='''# objects.all() displays all the questions in the database. <br>
+    >> > Question.objects.all()<br>''' + html.escape(str(Question.objects.all()))
+
+    return HttpResponse(text)
+
+def part3_detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def part3_results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def part3_vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
+def part3_index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
