c03b9324e633e145c7c899e2eb4df4dd222eed6b
s.andriyaka <s.andriyaka@1plus1.tv>
2018-09-10 15:28:55 +0300

    add index html for django tutorials

--- polls/urls.py
+++ polls/urls.py
@@ -10,4 +10,9 @@ urlpatterns = [
     path('part3/<int:question_id>/', views.part3_detail, name='part3_detail'),
     path('part3/<int:question_id>/results/', views.part3_results, name='part3_results'),
     path('part3/<int:question_id>/vote/', views.part3_vote, name='part3_vote'),
+    path('part4/', views.part4_IndexView.as_view(), name='part4_index'),
+    path('part4/<int:pk>/', views.part4_DetailView.as_view(), name='part4_detail'),
+    path('part4/<int:pk>/results/', views.part4_ResultsView.as_view(), name='part4_results'),
+    path('part4/<int:question_id>/vote/', views.part4_vote, name='part4_vote'),
+    path('',views.tutorials,name="tutorials")
 ]
